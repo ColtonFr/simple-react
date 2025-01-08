d039777be0422d37a75c6af60b711d2bc97211b5
coltonFr <colton@keenpayroll.ca>
2025-01-08 09:42:41 -0330

    added functionality: button counter, text submit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,75 @@
-function App() {
-  return (
-    <div>
-      <p>hello world</p>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ButtonComponent/>
+        <InputTextComponent/>
+      </div>
+    );
+  }
 }
 
-export default App;
+class ButtonComponent extends React.Component {
+  constructor (props) {
+
+    super(props);
+    this.state = {
+      count : 0
+    };
+
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+  }
+
+  increase() {
+    this.setState({count: this.state.count + 1});
+  };
+
+  decrease() {
+    this.setState({count: this.state.count - 1});
+  }
+
+  render() {
+
+    return (
+      <div>
+        <button onClick={this.increase}>Increase</button>
+        <button onClick={this.decrease}>Decrease</button>
+        <h1>{this.state.count}</h1>
+      </div>
+    )
+  }
+}
+
+class InputTextComponent extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      text: "",
+      submittedText: ""
+    };
+  }
+
+  handleChange(event) {
+    this.setState({text: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({submittedText: this.state.text});
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit = {this.handleSubmit}>
+          <input type = "text" onChange = {this.handleChange}/>
+          <button type = "submit">Submit</button>
+        </form>
+        <h1>{this.state.text}</h1>
+      </div>
+    )
+  }
+}
+
